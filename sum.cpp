24852c182a641977c0601e4c2057968d19f58dbe
Alexey Chubukov <alexchubukov12345@gmail.com>
2019-03-03 16:38:42 +0300

    new function on master

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+int summary(const int &a, const int &b){
+    return a + b;
+}
+
+int sub(const int &a, const int &b){
+    return a - b;
+}
+
+int main(){
+    std::cout << "Summary of 5 and 2 is 7" << std::endl;
+    return 0;
+}
+
